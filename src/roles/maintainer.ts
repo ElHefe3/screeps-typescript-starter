e23ea9dc7a6cf0a84289950d8553aff0225c19f7
ElHefe3 <71734422+ElHefe3@users.noreply.github.com>
2024-02-22 22:56:25 +0200

    bug/fix-maintainer

--- src/roles/maintainer.ts
+++ src/roles/maintainer.ts
@@ -1,16 +1,16 @@
 /** @param {Creep} creep **/
 
+import { scavengerAttribute } from "attributes";
+
 export const roleMaintainer = {
     run(creep: Creep): void {
-        const closestDamagedStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < ( structure.hitsMax * 0.8 )
-        });
+        const currentStructureTarget = Game.getObjectById(creep.memory.structureBeingRepaired ?? '') as Structure;
+        const isWall = currentStructureTarget?.structureType === STRUCTURE_WALL;
 
-        if(closestDamagedStructure) {
-            if(creep.repair(closestDamagedStructure) == ERR_NOT_IN_RANGE) {
-                creep.travelTo(closestDamagedStructure);
-            }
-        }
+        const currentStructureTargetIsDamaged = currentStructureTarget &&
+            currentStructureTarget?.hits < (isWall
+                ? ( currentStructureTarget.hitsMax * 0.8 )
+                : ( currentStructureTarget.hitsMax * 0.01 ));
 
         if(creep.memory.maintaining && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.maintaining = false;
@@ -28,31 +28,20 @@ export const roleMaintainer = {
 
         structuresToRepair.sort((a,b) => a.hits - b.hits);
 
-        if(creep.memory.building) {
+        structuresToRepair = (currentStructureTargetIsDamaged ? [currentStructureTarget, ...structuresToRepair] : structuresToRepair) as AnyStructure[];
+
+        creep.memory.structureBeingRepaired = structuresToRepair[0]?.id;
+
+        if(creep.memory.maintaining) {
+                creep.say('🛠️ fixin\'');
                 if(structuresToRepair.length > 0) {
                     if(creep.repair(structuresToRepair[0]) == ERR_NOT_IN_RANGE) {
                         creep.travelTo(structuresToRepair[0]);
                     }
                 }
         } else {
-            var containers = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER ||
-                            structure.structureType == STRUCTURE_STORAGE) &&
-                            structure.store[RESOURCE_ENERGY] > 0;
-                }
-            });
-
-            if(containers.length > 0) {
-                if(creep.withdraw(containers[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.travelTo(containers[0]);
-                }
-            } else {
-                var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-                if(sources.length > 0 && creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.travelTo(sources[0]);
-                }
-            }
+            creep.say('🔄 gaterin\'');
+            scavengerAttribute(creep);
         }
     }
 }
